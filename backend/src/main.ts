8f36d44aa6a8bbcdb703792001349a21d12580f2
ShehaniWageesha <it19113746@my.sliit.lk>
2022-11-19 23:08:16 +0530

    Backend Completed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+// import * as express from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
@@ -9,21 +9,21 @@ import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(server), { cors : true });
-   //const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors : true });
+  // const server = express();
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(server), { cors : true });
+   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors : true });
   await app.init();
   app.enableCors();
 
-  const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, '../cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '../cert', 'cert.pem')),
-  },server)
+  // const sslServer = https.createServer({
+  //   key: fs.readFileSync(path.join(__dirname, '../cert', 'key.pem')),
+  //   cert: fs.readFileSync(path.join(__dirname, '../cert', 'cert.pem')),
+  // },server)
 
   //HTTP 
-  //await app.listen(process.env.PORT || 3000); 
+  await app.listen(process.env.PORT || 3000); 
 
   //HTTPS
-  sslServer.listen(process.env.PORT || 3000, () => console.log("Secure Server on port 3000"));
+  // sslServer.listen(process.env.PORT || 3000, () => console.log("Secure Server on port 3000"));
 }
 bootstrap();
